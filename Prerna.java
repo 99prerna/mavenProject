b8bcefa008ddab316a88e0531d991c469be806bf
Prerna <prernachopra992002@gmail.com>
2024-08-16 11:58:11 +0530

    first commit

---
+++ Prerna.java
@@ -0,0 +1,7 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        int a,b = 30, 50;
+        System.out.println(a+b);
+        System.out.println("helloWorld");
+    }
+}
